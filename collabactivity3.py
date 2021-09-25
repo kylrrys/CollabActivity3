571a1d0a6d7027c9ddabd54bfae31401a870582b
Tumbokon, Virtucio, Villena <alectumbokon@gmail.com>
2021-09-25 12:45:39 +0000

    Collab Tumbokon, Virtucio, Villena

--- collabactivity3.py
+++ collabactivity3.py
@@ -21,9 +21,90 @@ if (mov=='A' or mov=='a'):
     else:
         d=200*.15
         tp=200-d
+    print("")
+    print("Choose your snack")
+    print("")
+    print("1. Popcorn with Drinks")
+    print("2. Hotdog with Drinks")
+    print("3. Chips with Drinks")
+    print("")
+    snack=int(input("Enter here: "))
+    print("")
+
+    if snack == 1:
+        print("Choose a size")
+        print("")
+        print("1. Large")
+        print("2. Medium")
+        print("3. Small")
+        print("")
+        size=int(input("Enter here: "))
+        print("")
+        if size == 1:
+            print("Your snack is a Large size Popcorn with Drinks")
+            fee = 180
+        elif size == 2:
+            print ("Your snack is a Medium size Popcorn with Drinks")
+            fee = 130
+        elif size == 3:
+            print ("Your snack is a Small size Popcorn with Drinks")
+            fee = 70
+        else: 
+            print ("Invalid Input")
+
+    elif snack == 2:
+        print ("Choose your Hotdog")
+        print ("")
+        print ("1. Original")
+        print ("2. Footlong")
+        print ("3. Hotdog with Cheese")
+        print ("4. Jumbo Hotdog")
+        print ("")
+        type=int(input("Enter here: "))
+        print ("")
+        if type == 1:
+            print ("Your snack is a Hotdog with Drinks")
+            fee = 60
+        elif type == 2:
+            print ("Your snack is a Footlong with Drinks")
+            fee = 100
+        elif type == 3:
+            print ("Your snack is a Hotdog with cheese with Drinks")
+            fee = 70
+        elif type == 4:
+            print ("Youe snack is a Jumbo Hotdog with Drinks")
+            fee = 80
+        else:
+            print ("Invalid Input")
+
+    elif snack == 3:
+        print ("Choose Your Chips")
+        print ("")
+        print ("1. Piatos")
+        print ("2. Nova")
+        print ("3. V-cut" )
+        print("")
+        chips = int(input("Enter Here: "))
+        if chips == 1:
+            print ("Your snacks is a Piatos with drinks")
+            fee = 60
+        elif chips == 2:
+            print ("Your snacks is a Nova with drinks")
+            fee = 60
+        elif chips == 3:
+            print ("Your snack is a V-cut with drinks")
+            fee = 60
+
+    else:
+        print("Invalid Input")
+
+    ff =(tp+fee)
+
     print("MOVIE A - Avengers: Endgame")
     print("Age: ", age)
     print("Ticket Price : ",tp)
+    print ("Your Total Fee is {} ".format(tp+fee))
+
 
 elif (mov=='B' or mov=='b'):
     if (age<=7):
@@ -36,9 +117,88 @@ elif (mov=='B' or mov=='b'):
     else:
         d=200*.15
         tp=200-d
+    print("")
+    print("Choose your snack")
+    print("")
+    print("1. Popcorn with Drinks")
+    print("2. Hotdog with Drinks")
+    print("3. Chips with Drinks")
+    print("")
+    snack=int(input("Enter here: "))
+    print("")
+
+    if snack == 1:
+        print("Choose a size")
+        print("")
+        print("1. Large")
+        print("2. Medium")
+        print("3. Small")
+        print("")
+        size=int(input("Enter here: "))
+        print("")
+        if size == 1:
+            print("Your snack is a Large size Popcorn with Drinks")
+            fee = 180
+        elif size == 2:
+            print ("Your snack is a Medium size Popcorn with Drinks")
+            fee = 130
+        elif size == 3:
+            print ("Your snack is a Small size Popcorn with Drinks")
+            fee = 70
+        else: 
+            print ("Invalid Input")
+
+    elif snack == 2:
+        print ("Choose your Hotdog")
+        print ("")
+        print ("1. Original")
+        print ("2. Footlong")
+        print ("3. Hotdog with Cheese")
+        print ("4. Jumbo Hotdog")
+        print ("")
+        type=int(input("Enter here: "))
+        print ("")
+        if type == 1:
+            print ("Your snack is a Hotdog with Drinks")
+            fee = 60
+        elif type == 2:
+            print ("Your snack is a Footlong with Drinks")
+            fee = 100
+        elif type == 3:
+            print ("Your snack is a Hotdog with cheese with Drinks")
+            fee = 70
+        elif type == 4:
+            print ("Youe snack is a Jumbo Hotdog with Drinks")
+            fee = 80
+        else:
+            print ("Invalid Input")
+
+    elif snack == 3:
+        print ("Choose Your Chips")
+        print ("")
+        print ("1. Piatos")
+        print ("2. Nova")
+        print ("3. V-cut" )
+        print("")
+        chips = int(input("Enter Here: "))
+        if chips == 1:
+            print ("Your snacks is a Piatos with drinks")
+            fee = 60
+        elif chips == 2:
+            print ("Your snacks is a Nova with drinks")
+            fee = 60
+        elif chips == 3:
+            print ("Your snack is a V-cut with drinks")
+            fee = 60
+
+    else:
+        print("Invalid Input")
+
+    ff =(tp+fee)
     print("MOVIE B- Godzilla vs Kong")
     print("Age: ", age)
     print("Ticket Price : ",tp)
+    print ("Your Total Fee is {} ".format(tp+fee))
 elif (mov=='C' or mov=='c'):
     if (age<=7):
         tp=0
@@ -50,9 +210,88 @@ elif (mov=='C' or mov=='c'):
     else:
         d=200*.15
         tp=200-d
+    print("")
+    print("Choose your snack")
+    print("")
+    print("1. Popcorn with Drinks")
+    print("2. Hotdog with Drinks")
+    print("3. Chips with Drinks")
+    print("")
+    snack=int(input("Enter here: "))
+    print("")
+
+    if snack == 1:
+        print("Choose a size")
+        print("")
+        print("1. Large")
+        print("2. Medium")
+        print("3. Small")
+        print("")
+        size=int(input("Enter here: "))
+        print("")
+        if size == 1:
+            print("Your snack is a Large size Popcorn with Drinks")
+            fee = 180
+        elif size == 2:
+            print ("Your snack is a Medium size Popcorn with Drinks")
+            fee = 130
+        elif size == 3:
+            print ("Your snack is a Small size Popcorn with Drinks")
+            fee = 70
+        else: 
+            print ("Invalid Input")
+
+    elif snack == 2:
+        print ("Choose your Hotdog")
+        print ("")
+        print ("1. Original")
+        print ("2. Footlong")
+        print ("3. Hotdog with Cheese")
+        print ("4. Jumbo Hotdog")
+        print ("")
+        type=int(input("Enter here: "))
+        print ("")
+        if type == 1:
+            print ("Your snack is a Hotdog with Drinks")
+            fee = 60
+        elif type == 2:
+            print ("Your snack is a Footlong with Drinks")
+            fee = 100
+        elif type == 3:
+            print ("Your snack is a Hotdog with cheese with Drinks")
+            fee = 70
+        elif type == 4:
+            print ("Youe snack is a Jumbo Hotdog with Drinks")
+            fee = 80
+        else:
+            print ("Invalid Input")
+
+    elif snack == 3:
+        print ("Choose Your Chips")
+        print ("")
+        print ("1. Piatos")
+        print ("2. Nova")
+        print ("3. V-cut" )
+        print("")
+        chips = int(input("Enter Here: "))
+        if chips == 1:
+            print ("Your snacks is a Piatos with drinks")
+            fee = 60
+        elif chips == 2:
+            print ("Your snacks is a Nova with drinks")
+            fee = 60
+        elif chips == 3:
+            print ("Your snack is a V-cut with drinks")
+            fee = 60
+
+    else:
+        print("Invalid Input")
+
+    ff =(tp+fee)
     print("MOVIE C - Fast and Furious 9 ")
     print("Age: ", age)
     print("Ticket Price : ",tp)
+    print ("Your Total Fee is {} ".format(tp+fee))
 elif (mov=='D' or mov=='d'):
     if (age<=7):
         tp=0
@@ -64,11 +303,93 @@ elif (mov=='D' or mov=='d'):
     else:
         d=200*.15
         tp=200-d
+    print("")
+    print("Choose your snack")
+    print("")
+    print("1. Popcorn with Drinks")
+    print("2. Hotdog with Drinks")
+    print("3. Chips with Drinks")
+    print("")
+    snack=int(input("Enter here: "))
+    print("")
+
+    if snack == 1:
+        print("Choose a size")
+        print("")
+        print("1. Large")
+        print("2. Medium")
+        print("3. Small")
+        print("")
+        size=int(input("Enter here: "))
+        print("")
+        if size == 1:
+            print("Your snack is a Large size Popcorn with Drinks")
+            fee = 180
+        elif size == 2:
+            print ("Your snack is a Medium size Popcorn with Drinks")
+            fee = 130
+        elif size == 3:
+            print ("Your snack is a Small size Popcorn with Drinks")
+            fee = 70
+        else: 
+            print ("Invalid Input")
+
+    elif snack == 2:
+        print ("Choose your Hotdog")
+        print ("")
+        print ("1. Original")
+        print ("2. Footlong")
+        print ("3. Hotdog with Cheese")
+        print ("4. Jumbo Hotdog")
+        print ("")
+        type=int(input("Enter here: "))
+        print ("")
+        if type == 1:
+            print ("Your snack is a Hotdog with Drinks")
+            fee = 60
+        elif type == 2:
+            print ("Your snack is a Footlong with Drinks")
+            fee = 100
+        elif type == 3:
+            print ("Your snack is a Hotdog with cheese with Drinks")
+            fee = 70
+        elif type == 4:
+            print ("Youe snack is a Jumbo Hotdog with Drinks")
+            fee = 80
+        else:
+            print ("Invalid Input")
+
+    elif snack == 3:
+        print ("Choose Your Chips")
+        print ("")
+        print ("1. Piatos")
+        print ("2. Nova")
+        print ("3. V-cut" )
+        print("")
+        chips = int(input("Enter Here: "))
+        if chips == 1:
+            print ("Your snacks is a Piatos with drinks")
+            fee = 60
+        elif chips == 2:
+            print ("Your snacks is a Nova with drinks")
+            fee = 60
+        elif chips == 3:
+            print ("Your snack is a V-cut with drinks")
+            fee = 60
+
+    else:
+        print("Invalid Input")
+
+    ff =(tp+fee)
     print("MOVIE D - Lion King ")
     print("Age: ", age)
     print("Ticket Price : ",tp)
+    print ("Your Total Fee is {} ".format(tp+fee))
 else:
     print("Invalid Input")
+
+
+
 
 
 
